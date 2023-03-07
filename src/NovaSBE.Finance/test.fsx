24d9f3636d3ca84a6e467e54a2ae91c88ffd4b29
Nicholas Hirschey <nhirschey@users.noreply.github.com>
2023-03-07 08:21:31 +0000

    initial

---
+++ src/NovaSBE.Finance/test.fsx
@@ -0,0 +1,7 @@
+#i """nuget: C:\Users\Nicholas Hirschey\Documents\github\NovaSBE.Finance\src\NovaSBE.Finance\bin\Debug"""
+
+#r "nuget: NovaSBE.Finance"
+
+open NovaSBE.Finance.French
+
+let ff3 = getFF3 Frequency.Daily
